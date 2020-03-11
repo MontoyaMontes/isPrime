c41b8d9b1aedce803c00d9cc534f44e51278d9af
Montoya Montes P <pmontoyam@ciencias.unam.mx>
2020-03-11 00:20:50 -0600

    Document and fixed

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
-//package isPrime;
-
+/**
+ * Montoya Montes Pedro
+ * 
+ * Clase principal, que prueba isPrime con 1 y 8 hilos con un valor.
+ *
+ */
 public class Main {
 
 	public static void main(String[] a) throws InterruptedException {
 		boolean respuesta1;
 		boolean respuesta2;
 
-        int prueba = 104543;//Número que queremos decidir si es o no un número primo.
+		int prueba = 104543;				//Número que queremos decidir si es o no un número primo.
 
-		long iniSec = System.currentTimeMillis();
-        isPrime secuencial = new isPrime(1);
-        //respuesta1 = secuencial.isPrime(1045430); 
-        respuesta1 = secuencial.isPrime(prueba);
-        long finSec = System.currentTimeMillis();
+		isPrime secuencial = new isPrime(1); 		//Iniciamos isPrime con un hilo.
+		respuesta1 = secuencial.isPrime(prueba);
 
-        long tiempo01 = finSec - iniSec;
-
-        long iniPar = System.currentTimeMillis();
-        isPrime paralelo = new isPrime(8);
-        respuesta2 = paralelo.isPrime(prueba);
-        long finPar = System.currentTimeMillis();
-
-        long tiempo02 = finPar -iniPar;
-      
-        System.out.println("Comparacion ejercicio 01");
-        System.out.println("El primo a comparar es el numero: "+ prueba);
-        System.out.println("El tiempo en el algoritmo secuencial tarda: " + tiempo01 + respuesta1);
-        System.out.println("El tiempo en el algoritmo paralelo tarda: " + tiempo02 + respuesta2);
+		isPrime paralelo = new isPrime(8); 		//Iniciamos isPrime con 8 hilos.
+		respuesta2 = paralelo.isPrime(prueba);
+	      
+		System.out.println("El valor a comparar es el número: \n"+ prueba);
+		System.out.println("El resultado secuencial es: \n"  + respuesta1);
+		System.out.println("El resultado paralelo es: \n" + respuesta2);
 	}
 }
--- isPrime.java
+++ isPrime.java
@@ -1,66 +1,84 @@
 import java.lang.Math;
 
+/**
+ * Montoya Montes Pedro
+ * 
+ * Clase isPrime, que dado un número n calcula si es primo o no con ayuda de hilos y teorema de Teoria de números.
+ *
+ * los hilos pueden ser cambiados en su constructor y busca cualquier valor que divida a la raíz del número a veriticar
+ *
+ */
 public class isPrime implements Runnable{
 
 	private int threads;
 	private static int numPrimo;
 	public static boolean result;
-	public static int longitudSubInter;
+	public static int intervalLength;
 	public static boolean[] answerArray;
+
+	/**
+	 * Constructor isPrime que si no se reciben parámetros, inicia con un solo hilo.
+	 */
 
 	public isPrime() {
 		this.threads = 1;
 	}
 
-	/**Método isPrime que, dado un número threads, crealo dicha cantidad de hilos.*/
+	/**
+	 * Constructor isPrime que, dado un número threads, crealo dicha cantidad de hilos.
+	 */
 	public isPrime(int threads) {
-		this.threads = threads > 1 ? threads : 1;
+		this.threads = threads > 1 ? threads : 1;	//Si el valor es menor a 1, solo crea un solo hilo.
 	}
 
 	/** Método isPrime, que dado un entero n, decide si n es primo o no.
-	  * @return True si n es primo, False en otro caso.
+	  * @return true si n es primo.
+	  * @return false si n es compuesto.
 	  */
 	public boolean isPrime(int n) throws InterruptedException{
-		result = true;                          //Por defecto el valor es 'true' (si se llega a ser divisible entre algún i, se cambia).
+		result = true;                          	//Por defecto el valor es 'true' (si se llega a ser divisible entre algún i, se cambia).
+		numPrimo = n;
 		int sqrtPrime = (int)Math.ceil(Math.sqrt(n));//Sacamos la raíz del valor, le aplicamos la función techo, y "casting" a tipo int.
 
-		numPrimo = n;					//Guardamos el valor del número para posterior uso, funcion abs para evitar casos donde n es negativo.
-	    longitudSubInter = Math.abs((Integer) sqrtPrime/threads);//Los sub-intervalos donde se movera nuestros hilos.
-		answerArray = new boolean[threads];
+		intervalLength = Math.abs((Integer) sqrtPrime/threads);//Los sub-intervalos donde se movera nuestros hilos.
+		answerArray = new boolean[threads];			//Creamos un arreglo para evitar la sección critica.
 
-		if(numPrimo == 0 || numPrimo == 1 )
+		if(numPrimo < 2)							//Verificar si no es 0 o 1 el valor.
 			return false; 
 		
-		Runnable process = () -> {
-			//System.out.println("HM");
-			int idThread = Integer.valueOf(Thread.currentThread().getName()); 
+		Runnable process = () -> {//Lambda para evitar hacer uso de run()
+			int idThread = Integer.valueOf(Thread.currentThread().getName()); //Obtenemos el número de hilo.
 
-			if(longitudSubInter*(idThread+1) <= numPrimo){//Para evitar salirnos del rango y hacer calculos inecesarios.
+			if(intervalLength*(idThread+1) <= numPrimo){//Para evitar salirnos del rango y hacer calculos inecesarios.
 
+				int interval = (intervalLength * idThread);
 				/** FOR: inicia en el primer valor de nuestro subIntervalor; acaba al final del mismo;aumenta i */
-				for(int i = (longitudSubInter * idThread); i <(longitudSubInter * idThread)+ longitudSubInter ; i++){  
+				for(int i = interval; i < interval + intervalLength ; i++){  
 
 					if(i<2) 				//Quitamos el caso donde i es igual a cero, uno o dos.
 						i=3;
-					answerArray[idThread] = (numPrimo % i == 0) ? (false): true;
+
+					answerArray[idThread] = (numPrimo % i == 0) ? (false): true;//Si numPrimo es divisible entre cualquier valor i,
+																				//en la posición que corresponde al hilo lo hacemos false, true en otro caso.
 				}
 			}
 		};
 
-		//Cada sub-intervalo se procesará en los diferentes hilos.
-		
-		for(int i = 0; i < threads; i++){ ///Aquí se puede cambiar a una clase anonima con Runnable.
+		/**
+		 * Inicializamos los hilos y los procesamos.
+		 */
+		for(int i = 0; i < threads; i++){ //
 			Thread t = new Thread (process);
 			t.setName(String.valueOf(i));
 			t.start();
 			t.join();
 		}
 	
-		for (boolean b : answerArray) {
+		for (boolean b : answerArray) {//Recorremos el arreglo de respuestas, si alguna es false, hará a la variable result 'false'.
 			result = result&&b;
-			System.out.println("FOR RES:"+result);
 		}
 		return result;
 	}
+
 	public void run(){}
 }
